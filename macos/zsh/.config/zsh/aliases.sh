0759fea4dd971c454d8f7aa004d1dd0a8b5bdb9f
Charles Hsieh <charleshs91tw@gmail.com>
2025-07-01 10:11:53 +0800

    Add zsh aliases

---
+++ macos/zsh/.config/zsh/aliases.sh
@@ -0,0 +1,9 @@
+alias c=clear
+alias cat=bat
+alias gdu=gdu-go
+alias gs="git status"
+alias ls="eza --icons=always"
+alias lzg=lazygit
+# alias v="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
+alias vim=nvim
+alias xcode26="open -a /Applications/Xcode-26.0.0-Beta.app"
